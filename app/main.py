b96c69c05d473ecd9a4b8f2ec0520b7b905c475b
peter.chang <po45ke95@gmail.com>
2023-09-21 22:02:53 +0800

    Updated: remove load variable in file.

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
 import boto3
 from datetime import datetime, timedelta
 import logging
-import os
-from dotenv import load_dotenv
 
-## load .env file for aws credentials
-load_dotenv()
-region_name = os.getenv('REGION_NAME')
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-account_id = os.getenv('ACCOUNT_ID')
+# initialize data
+region_name = ''
+aws_access_key_id = ''
+aws_secret_access_key = ''
+account_id = ''
 
 # boto3 client intialization
 client = boto3.client(
@@ -115,9 +112,12 @@ if __name__ == "__main__":
 
     day_result = days(3)
     formatted_cost_data = "\n".join([f"日期: {item['日期']}, 費用: {item['費用']}" for item in day_result])
-    print("消費日期暨時間:",formatted_cost_data)
+    # print("消費日期暨時間:",formatted_cost_data)
+    logging.info(f'消費日期暨時間: {formatted_cost_data}')
 
     ratio = caluclate_last_two_days_ratio(day_result)
-    print("消費成長比:", ratio + '%')
+    # print("消費成長比:", ratio + '%')
+    logging.info(f'消費成長比: {ratio} %')
 
-    print("本月壘積消費:", monthly())
+    # print("本月壘積消費:", monthly())
+    logging.info(f'本月壘積消費: {monthly()}')
