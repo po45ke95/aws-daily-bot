cd442755aeb0fd212ae09572af21e67d2a72db53
Peter <po45ke95@gmail.com>
2023-09-22 17:16:43 +0800

    Updated: send data to telegram.
    Del: sensetive data.
    
    Updated: function description.
    
    Updated: fixed output format.
    
    Updated: README for Reference.
    
    Updated: remove send_telegram_message parse_mode config.
    
    Del: remove sensitve data.

--- README.md
+++ README.md
@@ -11,4 +11,11 @@
 - aws_access_key_id
 - aws_secret_access_key
 - aws_account_id (可於 aws 帳號中獲取)
+- telegram_token (自行於 telegram bot father 中建立)
+- chat_id (於 telegram bot api request 中可以獲取)
 - (aws access_key, secret_access_key 都可以透過 aws 生成 請記得給予查詢帳單的相關權限)
+
+## Reference
+- [aws boto3](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ce.html) 
+- [aws cost api doc](https://docs.aws.amazon.com/aws-cost-management/latest/APIReference/API_Operations_AWS_Cost_Explorer_Service.html)
+- [telegram api](https://docs.python-telegram-bot.org/en/v20.5/index.html)
--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 import boto3
 from datetime import datetime, timedelta
 import logging
+import requests
 
 # initialize data
 region_name = ''
 aws_access_key_id = ''
 aws_secret_access_key = ''
 account_id = ''
+telegram_token = ''
+chat_id = ''
 
 # boto3 client intialization
 client = boto3.client(
@@ -19,6 +22,8 @@ client = boto3.client(
 # log initialization
 logging.basicConfig(level=logging.INFO , format='%(asctime)s - %(levelname)s - %(message)s')
 
+
+# daiy fuction
 def days(days):
 
     end = datetime.now().strftime('%Y-%m-%d')
@@ -56,17 +61,19 @@ def days(days):
         cost_data.append(cost_detail)
 
     # remove latest -1 days data
-    dis = len(cost_data) - 1
-    cost_data.pop(dis)
+    # dis = len(cost_data) - 1
+    # cost_data.pop(dis)
 
     return cost_data
 
+
+# calculate last two day pricing functions
 def caluclate_last_two_days_ratio(cost_data):
     if len(cost_data) < 2:
         return "Insufficient data to calculate ratio"
     
-    last_day_cost_str = cost_data[-1]['費用'].split(' ')[0]
-    second_last_day_cost_str = cost_data[-2]['費用'].split(' ')[0]
+    last_day_cost_str = cost_data[-2]['費用'].split(' ')[0]
+    second_last_day_cost_str = cost_data[-3]['費用'].split(' ')[0]
 
     last_day_cost = float(last_day_cost_str)
     second_last_day_cost = float(second_last_day_cost_str)
@@ -81,6 +88,8 @@ def caluclate_last_two_days_ratio(cost_data):
     else:
         return "Cannot calculate ratio: cost for the second last day is zero."
 
+
+# calculate monthly fucntion
 def monthly():
     end = datetime.now().strftime('%Y-%m-%d')
     start = datetime.now().strftime('%Y-%m-01')
@@ -108,16 +117,36 @@ def monthly():
         cost_now += round(float(cost_str),2)
     return cost_now
 
+
+# send telegram message function
+def send_telegram_message(chat_id, text):
+    base_url = f'https://api.telegram.org/bot{telegram_token}/sendMessage'
+    payload = {
+        "chat_id": chat_id,
+        "text": text,
+    }
+    response = requests.post(base_url, data=payload)
+    return response.json()
+
+
+# grouping data of telegram messages function
+def send_total_message():
+    day_result = days(3)
+    formatted_data = "\n".join([f"日期: {item['日期']} , 費用: {item['費用']}" for item in day_result])
+    ratio = caluclate_last_two_days_ratio(day_result)
+    monthly_data = monthly()
+
+    final = (
+        f'AWS_ACCOUNT: {account_id}' + '\n'
+        f'AWS_REGION: {region_name}' + '\n'
+        f'消費日期暨時間: \n{formatted_data}' + ' (昨日尚未出帳完成僅供參考)' + '\n'
+        f'消費成長比: {ratio} % (累積計算至出帳完成，不含昨日)' + '\n'
+        f'本月壘積消費: {monthly_data} USD (包含未出帳數據總和)'
+    )
+    send_telegram_message(chat_id, final)
+    return final
+
 if __name__ == "__main__":
 
-    day_result = days(3)
-    formatted_cost_data = "\n".join([f"日期: {item['日期']}, 費用: {item['費用']}" for item in day_result])
-    # print("消費日期暨時間:",formatted_cost_data)
-    logging.info(f'消費日期暨時間: {formatted_cost_data}')
-
-    ratio = caluclate_last_two_days_ratio(day_result)
-    # print("消費成長比:", ratio + '%')
-    logging.info(f'消費成長比: {ratio} %')
-
-    # print("本月壘積消費:", monthly())
-    logging.info(f'本月壘積消費: {monthly()}')
+    send_total_message()
+    logging.info(f'Send pricing data to telegram.')
